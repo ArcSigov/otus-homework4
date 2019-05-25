3b74ff3d6630126e8661057c04e455d44663d013
Arcady Sigov <48837966+ArcSigov@users.noreply.github.com>
2019-05-26 00:02:49 +0300

    Update print_ip.cpp

--- print_ip.cpp
+++ print_ip.cpp
@@ -6,20 +6,22 @@
 #include "print_ip.h"
 
 
-std::vector<int> _vector{110,111,112,113};
-std::list <int > _list{99,98,97,96};
-std::string _string {"87.88.89.90"};
-const int _int = 2130706433;
-const long long _long = 8875824491850138409;
-const unsigned short _short = 0;
-const char _char = -1;
-auto _tuple = std::make_tuple("121","125","255","254");
+std::vector<int> _vector{110,111,112,113};              ///< Целочисленный вектор ip-адреса
+std::list <int > _list{99,98,97,96};                    ///< Целочисленный лист ip-адреса
+std::string _string {"87.88.89.90"};                    ///< Строка с ip-адресом
+const int _int = 2130706433;                            ///< ip-адрес в int представлении
+const long long _long = 8875824491850138409;            ///< ip-адрес в long представлении 
+const unsigned short _short = 0;                        ///< ip-адрес в ushort представлении
+const char _char = -1;                                  ///< ip-адрес в char представлении
+auto _tuple = std::make_tuple("121","125","255","254"); ///< однородный кортеж ip-адреса
 #ifdef DEBUG
-auto _tuple_bad = std::make_tuple("1",'1',2, 4. );
+auto _tuple_bad = std::make_tuple("1",'1',2, 4. );      ///< разнородный кортеж ip-адреса (только для debug версии)
 #endif
 
 
-
+/*!
+  Основная функция
+*/
 int main()
 {
   ip_printer(_char);
